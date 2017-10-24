6652908966cde07ded28cd0a64bcd4990c1ff615
icehap001 <icehap001@gmail.com>
2017-10-25 01:07:12 +0700

    p14

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
-namespace Lab01
+namespace RandomNumber
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
-           
+            Random random = new Random();
+            int randomNumber = random.Next(0, 9);
+            Console.WriteLine(randomNumber);
+
+            Console.Write("Please enter value 1 :");
+            int asd = Convert.ToInt32(Console.ReadLine());
+
+            bool a = (randomNumber == asd);
+            Console.WriteLine("{0}", a);
+
         }
     }
+
+
 }
