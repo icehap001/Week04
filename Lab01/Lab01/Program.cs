173730f4f74afc819dbf8019ee8e8a3f50530cc0
icehap001 <icehap001@gmail.com>
2017-10-25 02:32:56 +0700

    213

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
-namespace Lab01
+public class MathTest
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        for (float i = -2; i < Math.PI * 4.5F; i += 0.35F)
         {
-           
+            Console.WriteLine("The tan {0,10:F} = {1,-10:F6}" +
+            spaces(Math.Tan(i)) + "*", i, Math.Tan(i));
         }
+    }
+    private static string spaces(double val)
+    {
+        string SpaceString = new String(' ', ((int)(val *0.50)) + 10);
+        return SpaceString;
     }
 }
